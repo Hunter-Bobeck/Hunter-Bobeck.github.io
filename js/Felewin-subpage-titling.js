0327f494b026b5e89b3e9d8eb57e08411ef12227
Hunter-Bobeck <hdbipod@gmail.com>
2018-12-29 22:27:30 -0500

    Update Felewin-subpage-titling.js

--- js/Felewin-subpage-titling.js
+++ js/Felewin-subpage-titling.js
@@ -1,5 +1,10 @@
 $(document).ready(function()
 {
-	let fileNameSpaced = (location.pathname.substring(location.pathname.lastIndexOf("/") + 1)).replace(".html", "").replace(/-/g, ' ');
-	document.title += (((fileNameSpaced === "index") || !fileNameSpaced.includes(".html")) ? "" : " | "+fileNameSpaced);
+	let filePath = location.pathname;
+	let fileNameStartIndex = filePath.lastIndexOf("/") + 1;
+	let fileTitle = filePath.substring(fileNameStartIndex);
+	let fileName = fileTitle.replace(".html", "");
+	let fileNameSpaced = fileName.replace(/-/g, ' ');
+
+	document.title += (((fileNameSpaced === "index") || !fileTitle.includes(".html")) ? "" : " | "+fileNameSpaced);
 });
